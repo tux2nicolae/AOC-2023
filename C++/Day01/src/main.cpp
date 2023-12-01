c148d8b45bc08dbe7ec41ec4b2ed47fff65694b6
Telechi Nicolae <tux2nicolae@gmail.com>
2023-12-01 08:29:27 +0200

    Add day 1

--- C++/Day01/src/main.cpp
+++ C++/Day01/src/main.cpp
@@ -33,40 +33,95 @@ using namespace std;
 #include "../../AOCLib/src/Time.h"
 #include "../../AOCLib/src/Util.h"
 
+vector<pair<string, string>> digits{ {
+  { "one", "1" },
+  { "two", "2" },
+  { "three", "3" },
+  { "four", "4" },
+  { "five", "5" },
+  { "six", "6" },
+  { "seven", "7" },
+  { "eight", "8" },
+  { "nine", "9" },
+} };
+
+void Replace(string & line, size_t offset)
+{
+  for (auto [word, number] : digits)
+  {
+    if (line.substr(offset)._Starts_with(word))
+    {
+      line.replace(offset, word.size(), number);
+    }
+  }
+}
+
+void ReplaceDigits(string & line)
+{
+  for (int offset = 0; offset < line.size(); offset++)
+  {
+    Replace(line, offset);
+  }
+}
+
+void ReplaceDigitsBackward(string & line)
+{
+  for (int offset = line.size() - 1; offset >= 0; offset--)
+  {
+    Replace(line, offset);
+  }
+}
+
 int main()
 {
   ifstream in("..\\src\\_input.in");
-  // ofstream out("..\\src\\_output.out");
+  ofstream out("..\\src\\_output.out");
 
   FStreamReader  reader(in);
-  vector<string> v = reader.ReadLines();
-
-  vector<int> e;
+  vector<string> v         = reader.ReadLines();
+  vector<string> vBackward = v;
 
   int s = 0;
-  // part 1
-  for (const auto & line : v)
+
+  // part 2
+  for (auto & line : v)
   {
-    if (line.empty())
-    {
-      e.push_back(s);
-      s = 0;
-    }
-    else
-    {
-      s += stoi(line);
-    }
+    ReplaceDigits(line);
   }
 
-  e.push_back(s);
+  for (auto & line : vBackward)
+  {
+    ReplaceDigitsBackward(line);
+  }
 
-  sort(begin(e), end(e), greater<int>());
+  // part 1
+  for (int i = 0; i < v.size(); i++)
+  {
+    auto line         = v[i];
+    auto lineBackward = vBackward[i];
 
-  cout << e[0] + e[1] + e[2];
+    auto pos = line.find_first_of("123456789");
+    assert(pos != string::npos);
+    int first = line[pos] - '0';
+
+    pos = lineBackward.find_last_of("123456789");
+    assert(pos != string::npos);
+    int last = lineBackward[pos] - '0';
+
+    assert(first);
+    assert(last);
+
+    s += first * 10 + last;
+  }
+
+  cout << s;
 
   // out
-  // FStreamWriter writer(out);
-  // writer.WriteVector(v);
+  FStreamWriter writer(out);
+  for (const auto & element : v)
+  {
+    out << element << endl;
+  }
 
   return 0;
 }
