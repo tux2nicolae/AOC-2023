61ec62139f35cdff1473875932055d315c529490
Nicolae Telechi <nicolae.telechi@caphyon.com>
2023-12-21 14:28:02 +0200

    Add part2 for day 20

--- C++/Day20/src/main.cpp
+++ C++/Day20/src/main.cpp
@@ -187,12 +187,6 @@ struct OutputModule : public Module
 
   void Receive(string from, Pulse pulse) override
   {
-    if (pulse.type == Pulse::Type::low)
-    {
-      cout << "plm";
-      return;
-    }
-
     // cout << from << " : " << static_cast<int>(pulse.type) << endl;
   }
 };
@@ -215,11 +209,52 @@ void Processor::Process()
     auto [module, from, pulse] = queue.front();
     queue.pop();
 
+    if (from == "rz" && pulse.type == Pulse::Type::high)
+    {
+      int xxx = 4057;
+    }
+
+    if (from == "lf" && pulse.type == Pulse::Type::high)
+    {
+      int xxx = 3911;
+    }
+
+    if (from == "br" && pulse.type == Pulse::Type::high)
+    {
+      int xxx = 3877;
+    }
+
+    if (from == "fk" && pulse.type == Pulse::Type::high)
+    {
+      int xxx = 4079;
+    }
+
     module->Receive(from, pulse);
 
     totalLowPulses += pulse.type == Pulse::Type::low;
     totalHighPulses += pulse.type == Pulse::Type::high;
   }
+}
+
+bool AllHight(string conjunctionModule)
+{
+  // update Conjunction Modules memory
+  auto module = modules[conjunctionModule].get();
+  assert(module);
+
+  assert(module->type == Module::Type::Conjunction);
+  if (module->type == Module::Type::Conjunction)
+  {
+    auto conjunctionModule = static_cast<ConjunctionModule *>(module);
+
+    return all_of(conjunctionModule->memory.begin(), conjunctionModule->memory.end(),
+                  [](const auto & item)
+                  {
+                    return item.second.type == Pulse::Type::high;
+                  });
+  }
+
+  return false;
 }
 
 int main()
@@ -272,9 +307,9 @@ int main()
 
   for (auto [to, fromVector] : graphBack)
   {
+    // output module
     if (!modules[to])
     {
-      // output module
       modules[to] = make_unique<OutputModule>(to);
     }
 
@@ -289,15 +324,114 @@ int main()
     }
   }
 
-  Button button;
-  for (size_t i = 1; i <= 1000; i++)
+  // vector<FlipFlopModule *> flipFlopModules;
+  // for (auto & [_, module] : modules)
+  //{
+  //   if (module->type == Module::Type::FlipFlop)
+  //   {
+  //     auto flipFlopModule = static_cast<FlipFlopModule *>(module.get());
+  //     flipFlopModules.push_back(flipFlopModule);
+  //   }
+  // }
+
+  string sortedFlipFlopModules =
+    "lz nn mj tx qn vq zc np qz fq ms bt dn xd tp bh hf hq nm gp cm qk hn mh xn zb xp fm hh kb gz "
+    "mq vf cr jd dq vx fl nc dj sf rq ds sx xv xb kz fn";
+
+  auto sortedFlipFlop = AOC::Explode(sortedFlipFlopModules, " ");
+  reverse(sortedFlipFlop.begin(), sortedFlipFlop.end());
+
+  vector<FlipFlopModule *> flipFlopModules;
+  for (auto & moduleName : sortedFlipFlop)
   {
-    button.Send();
+    auto module = modules[moduleName].get();
+    if (module->type == Module::Type::FlipFlop)
+    {
+      auto flipFlopModule = static_cast<FlipFlopModule *>(module);
+      flipFlopModules.push_back(flipFlopModule);
+    }
   }
+
+  // queue<pair<string, bool>> q;
+  // q.push({ "rx", false });
+
+  // unordered_map<string, bool> visited;
+
+  // while (!q.empty())
+  //{
+  //   auto [currentNode, currentState] = q.front();
+  //   q.pop();
+
+  //  for (auto from : graphBack[currentNode])
+  //  {
+  //    auto fromModule = modules[from].get();
+  //    if (fromModule->type == Module::Type::FlipFlop)
+  //    {
+  //      auto flipFlopModule   = static_cast<FlipFlopModule *>(fromModule);
+  //      flipFlopModule->state = !currentState;
+  //    }
+
+  //    if (!visited[from])
+  //    {
+  //      q.push({ from, !currentState });
+  //      visited[from] = true;
+  //    }
+  //  }
+  //}
+
+  unordered_map<string, int>  cicles;
+  unordered_map<string, bool> values;
+
+  for (auto module : flipFlopModules)
+  {
+    values[module->name] = module->state;
+  }
+
+  Button button;
+  for (size_t i = 1; i <= 10000; i++)
+  {
+    processor.totalLowPulses  = 0;
+    processor.totalHighPulses = 0;
+
+    button.Send();
+
+    for (auto module : flipFlopModules)
+    {
+      if (!cicles.count(module->name) && module->state)
+        cicles[module->name] = i;
+
+      out << /*module->name << " " <<*/ module->state /* << endl*/;
+    }
+
+    out << endl;
+
+    // auto rz = AllHight("rz");
+    // auto lf = AllHight("lf");
+    // auto br = AllHight("br");
+    //
+    // auto lbTest = AllHight("lb");
+    // auto lb     = fk && rz && lf && br;
+
+    /* out << "LOW:HIGH " << processor.totalLowPulses << " : " << processor.totalHighPulses << " = "
+         << processor.totalLowPulses + processor.totalHighPulses << endl;*/
+  }
+
+  sort(begin(flipFlopModules), end(flipFlopModules),
+       [&](FlipFlopModule * first, FlipFlopModule * second)
+       {
+         return cicles[first->name] < cicles[second->name];
+       });
+
+  for (auto module : flipFlopModules)
+  {
+    out << module->name << " ";
+  }
+
+  out << endl;
 
   cout << "LOW: " << processor.totalLowPulses << endl;
   cout << "HIGH:" << processor.totalHighPulses << endl;
-  cout << processor.totalLowPulses * processor.totalHighPulses;
+  cout << processor.totalLowPulses * processor.totalHighPulses << endl;
 
   // for (auto & [_, module] : modules)
   //{
@@ -306,6 +440,9 @@ int main()
   //     auto flipFlopModule = static_cast<FlipFlopModule *>(module.get());
   //   }
   // }
+
+  // too high -> 501848147836682
+  cout << AOC::Cmmmc(AOC::Cmmmc(4057, 3911), AOC::Cmmmc(3877, 4079));
 
   return 0;
 }
